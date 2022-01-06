fdb994d1f755c2758db1798ca5fbd631b588d7dc
Corbin-Rochelle <ctr233@msstate.edu>
2022-01-06 15:03:48 -0600

    Some edits

--- dirbephasefunction.py
+++ dirbephasefunction.py
@@ -19,25 +19,23 @@ cs1=cs0
 cs2=cs0
 n=cs0
 ## 1.25 microns
-cs0(1)=-0.942 
-cs1(1)=0.121  # backscatter!
-cs2(1)=-0.165  
-n(1)=3.22559  # normalization from ncalc (so integral over sphere = 1)
+cs0[1]=-0.942
+cs1[1]=0.121  # backscatter!
+cs2[1]=-0.165
+n[1]=3.22559  # normalization from ncalc (so integral over sphere = 1)
 # 2.2 microns
-cs0(2)=-0.527
-cs1(2)=0.187
-cs2(2)=-0.598
-n(2)=0.415966
+cs0[2]=-0.527
+cs1[2]=0.187
+cs2[2]=-0.598
+n[2]=0.415966
 # 3.5 microns
-cs0(3)=-0.431
-cs1(3)=0.172
-cs2(3)=-0.633
-n(3)=0.324797
+cs0[3]=-0.431
+cs1[3]=0.172
+cs2[3]=-0.633
+n[3]=0.324797
 
 
-theta=acos(costheta)
+theta=numpy.arccos(costheta)
 
+# What is band and pfunc, and why are they imported.
 pfunc=n(band)*(cs0(band) + cs1(band) * theta + numpy.exp(cs2(band)*theta))
-
-
-end
